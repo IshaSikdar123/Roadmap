2bd68ff5562878724d21a563b2ee8f5f114269d8
Poras-oss <55978642+Poras-oss@users.noreply.github.com>
2024-11-05 01:21:58 +0530

    .

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div>
 
-      {/* Will call api here for getting selected path information than will render the cities */}
+      {/* Will call api here for getting selected path information then we'll render the cities */}
 
     {/* When displaying customized roadmap the position should always start from 1 irrespective of city component */ }
         <Blue position={7} />
